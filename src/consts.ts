2aa79b2a5affa1f0533ddace4f39dcc3638044fd
Johao <johaoramirez@gmail.com>
2025-09-14 19:25:26 -0500

    Agregando repositorio

---
+++ src/consts.ts
@@ -0,0 +1,36 @@
+export const SITE_TITLE = "Johao";
+export const SITE_DESCRIPTION =
+  `Página web de Hernando Johao Ramirez Palomino, utilizada para subir sus estudios, investigaciones o proyectos personales y darse a conocer.`.trim();
+
+export const KNOWN_TECH = [
+  "Astro",
+  "Tailwind CSS",
+  "TypeScript",
+  "Node.JS",
+  "Express.JS",
+  "React",
+  "Next.JS",
+  "PHP", 
+  "MySQL",
+  "PostgreSQL",
+  "Python"
+];
+export const ABOUT_ME =
+  `Hola, mi nombre es Hernando Johao Ramirez Palomino, soy desarrollador de software y estudiantes en ingeniería de software. Mis estudios se basan en el Análisis y Desarrollo de Software y en la integración de productos o servicios escalables. He adquirido diversos conocimientos en lenguajes como JavaScript con NodeJS, Angular y React; además de esto también he hecho diversos proyectos pequeños con Java, Spring y PostgreSQL. Actualmente trabajo en un proyecto de gestión empresarial el cual contiene también el lenguaje de PHP.
+He tenido experiencia externa al estudio, dado mi interés individual por el sector, aprendiendo tecnologías y lenguajes como Python. Aprendizaje guiado por el interés de realizar proyectos personales. 
+Estoy entusiasmado por continuar mi carrera profesional y estoy buscando cada día una nueva enseñanza personal, laboral o académica en la que pueda extender y aplicar mis conocimientos. Mi objetivo es contribuir al éxito de un equipo dinámico y en crecimiento, mientras continúo desarrollándome como profesional en el sector del desarrollo de software.`.trim();
+export const GITHUB_USERNAME = "DopoDev";
+export const QUOTE = "El código es como el humor. Cuando tienes que explicarlo, es malo.";
+export const NAV_LINKS: Array<{ title: string; href?: string }> = [
+  {
+    title: "Blog",
+  },
+  {
+    title: "Github",
+    href: "//github.com/" + GITHUB_USERNAME,
+  },
+  {
+    title: "Source",
+    href: "//github.com/ArnavK-09/token-template",
+  },
+];
